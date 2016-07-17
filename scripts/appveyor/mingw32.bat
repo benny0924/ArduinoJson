501109fb2890a43e913bf705750d24a2ea8660fb
Benoit Blanchon <github@benoitblanchon.fr>
2016-07-17 17:33:34 +0200

    Move appveyor config into a .bat

---
+++ scripts/appveyor/mingw32.bat
@@ -0,0 +1,6 @@
+:: Workaround for CMake not wanting sh.exe on PATH for MinGW
+set PATH=C:\MinGW\bin;%PATH:C:\Program Files\Git\usr\bin;=%
+
+cmake -G "MinGW Makefiles" .
+cmake --build .
+bin\ArduinoJson.exe
---
+++ scripts/appveyor/visual-studio.bat
@@ -0,0 +1,3 @@
+cmake -DCMAKE_BUILD_TYPE=%CONFIGURATION% -G "Visual Studio %VS_VERSION%" .
+cmake --build . --config %CONFIGURATION%
+bin\%CONFIGURATION%\ArduinoJson.exe
